e26048ba4269235df6ce4661bd86370fe251e70a
Waldener Monteiro <jjrr019@gmail.com>
2021-01-16 23:59:35 -0300

    refactor(login): refactor css rules in login and add scoped attribute.

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,18 @@ export default {
   name: 'App'
 }
 </script>
+<style>
+.q-field__label {
+  color: #cbd0e0;
+  opacity: 0.6;
+}
+.q-field__control {
+  color: #cbd0e0 !important;
+  opacity: 0.6;
+  height: 20px;
+}
+.q-field--standard .q-field__control:before {
+  border-color: #cbd0e0 !important;
+  opacity: 0.6 !important;
+}
+</style>
