d05e21eafbf87865bee2825536b50ea0690522c1
Waldener Monteiro <jjrr019@gmail.com>
2021-01-18 21:35:17 -0300

    refactor: comentend some css code and modify q-item

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,28 @@ export default {
   name: 'App'
 }
 </script>
-<style>
+<style lang="scss">
 /* .q-field__label {
   color: #cbd0e0;
   /* opacity: 0.6;
 } */
-.q-field__control {
-  color: #cbd0e0 !important;
-  opacity: 0.6;
-  height: 20px;
+// .q-field__control {
+//   color: #cbd0e0 !important;
+//   opacity: 0.6;
+//   height: 20px;
+// }
+// .q-field--standard .q-field__control:before {
+//   border-color: #cbd0e0 !important;
+//   opacity: 0.6 !important;
+// }
+.q-item {
+  padding: 8px 10px !important;
+  border-radius: 10px;
 }
-.q-field--standard .q-field__control:before {
-  border-color: #cbd0e0 !important;
-  opacity: 0.6 !important;
+.text-grey {
+  color: $grey !important;
+}
+.text-secondary {
+  color: $secondary !important;
 }
 </style>
